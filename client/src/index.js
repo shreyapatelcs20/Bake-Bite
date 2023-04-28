1471be0bcd2e02f22980643fb9de17c522cac359
simplilearn <shreya.p@simplilearn.net>
2023-04-28 09:20:50 +0530

    done

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/auth";
+import { SearchProvider } from "./context/search";
+import { CartProvider } from "./context/cart";
+import "antd/dist/reset.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AuthProvider>
+    <SearchProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </SearchProvider>
+  </AuthProvider>
+);
+reportWebVitals();
